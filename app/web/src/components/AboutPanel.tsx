69e5a83ea2b7ee6f4413a573e2096c55e634c0f1
nesquikm <drapolyuk@gmail.com>
2024-12-01 14:40:30 +0400

    feat: enhance AboutPanel with Grid layout and Last Word icon

--- app/web/src/components/AboutPanel.tsx
+++ app/web/src/components/AboutPanel.tsx
@@ -3,6 +3,7 @@ import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import AboutDialog from "./AboutDialog";
+import Grid from "@mui/material/Grid2";
 
 export default function AboutPanel() {
   const [open, setOpen] = useState(false);
@@ -16,18 +17,23 @@ export default function AboutPanel() {
   }
 
   return (
-    <Paper sx={{ mt: 2, p:2 }}>
+    <Paper sx={{ mt: 2, p: 2 }}>
+      <Grid container>
         <Typography variant="subtitle2" component="div">
+          <img
+            src="/lastword.svg"
+            alt="Last Word"
+            style={{ width: "40px", marginRight: "10px" }}
+          />
           This is a small Solana-based game where players compete to have the
           last word on various topics. Users can create topics, pay to replace
           comments, and earn rewards when topics lock after a timeout. With
           growing costs for each comment and unique mechanics, the game blends
           strategy, greed, and fun!
         </Typography>
-        <Button onClick={moreInfo}>
-          More Info
-        </Button>
-        <AboutDialog open={open} onClose={onAboutDialogClose} />
+      </Grid>
+      <Button onClick={moreInfo}>More Info</Button>
+      <AboutDialog open={open} onClose={onAboutDialogClose} />
     </Paper>
   );
 }
